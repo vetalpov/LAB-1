a4cde2dd82b57dd227a3e020d2a189bec3fd7b28
vetalpov <117040614+vetalpov@users.noreply.github.com>
2023-04-07 15:59:01 +0300

    Add files via upload

---
+++ lab_1.3/lab_1.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 4, 2, 7, 1, 8, 3, 6, 5 };
+
+        var oddNumbers = numbers
+            .Where(n => n % 2 == 1) 
+            .Select(n => n.ToString())
+            .OrderBy(n => n); 
+
+        Console.WriteLine(string.Join(", ", oddNumbers)); 
+    }
+}
