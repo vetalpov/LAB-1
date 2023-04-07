4f4a292ea9d19a356f71e76f6a55b4bce3961c24
vetalpov <117040614+vetalpov@users.noreply.github.com>
2023-04-07 15:58:18 +0300

    Add files via upload

---
+++ lab_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = CreateRandomList(10);
+        Console.WriteLine("Список чисел:");
+        PrintList(numbers);
+
+        try
+        {
+            Console.Write("Введiть елекмент k, який потрiбно видалити: ");
+            int k = int.Parse(Console.ReadLine());
+
+            RemoveElement(numbers, k);
+            Console.WriteLine("Список чисел без k:");
+            PrintList(numbers);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Помилка: " + e.Message);
+        }
+
+        Console.ReadKey();
+    }
+
+    static List<int> CreateRandomList(int length)
+    {
+        List<int> numbers = new List<int>();
+        Random random = new Random();
+        for (int i = 0; i < length; i++)
+        {
+            numbers.Add(random.Next(100));
+        }
+        return numbers;
+    }
+
+    static void PrintList(List<int> numbers)
+    {
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        Console.WriteLine();
+    }
+
+    static void RemoveElement(List<int> numbers, int k)
+    {
+        if (k < 0 || k >= numbers.Count)
+        {
+            throw new ArgumentException("Не коректні дані");
+        }
+        for (int i = k; i < numbers.Count - 1; i++)
+        {
+            numbers[i] = numbers[i + 1];
+        }
+        numbers.RemoveAt(numbers.Count - 1);
+    }
+}
